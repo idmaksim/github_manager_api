f8fcc6e9bf78b2875945c67ce007a8598797255d
idmaksim <i@dmaksim.ru>
2024-05-23 10:45:53 +0300

    commit

---
+++ src/api/dependencies.py
@@ -0,0 +1,10 @@
+# dependencies.py
+
+from fastapi import Request, Depends, HTTPException
+from services.github import GithubService
+
+def get_github_service(request: Request):
+    access_token = request.headers.get('access-token')
+    if not access_token:
+        raise HTTPException(status_code=400, detail="Access token is missing")
+    return GithubService(access_token)
