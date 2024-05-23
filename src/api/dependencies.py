66c2dd99732a3e3aa7ab979c67c2deafeb493cb9
idmaksim <i@dmaksim.ru>
2024-05-23 18:01:37 +0300

    commit

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,6 +1,7 @@
 from fastapi import Request, HTTPException
 from services.github import GithubService
 
+
 def get_github_service(request: Request):
     access_token = request.headers.get('access-token')
     if not access_token:
