6d2e08438c9897febda62d8feee2b8004968d0a1
idmaksim <i@dmaksim.ru>
2024-05-26 13:15:24 +0300

    format endpoints

--- src/api/repository.py
+++ src/api/repository.py
@@ -30,7 +30,7 @@ async def add_repo(
     try:
         await service.create_repo(repository)
         return Response(status_code=status.HTTP_201_CREATED)
-    
+
     except Exception as e:
         await handle_route_error(e, status_code=status.HTTP_400_BAD_REQUEST)
 
